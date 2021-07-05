5639e5f49905c228604fa479585bd5c57123773a
igorplisko <igorplisko95@gmail.com>
2021-07-05 16:07:12 +0300

    commit

--- sneakes/src/App.js
+++ sneakes/src/App.js
@@ -3,9 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="wrapper">
-      <header>
-        <div className="headerLeft">
+    <div className="wrapper clear">
+      <header className="d-flex justify-between">
+        <div className="d-flex align-center">
           <img width={40} height={40} src="/img/logo.png" />
           <svg />
           <div className="headerInfo">
@@ -14,12 +14,12 @@ function App() {
           </div>
         </div>
         <ul className="headerRight">
-          <li>
+          <li className="mr-30">
             <img width={18} height={18} src="/img/cart.svg" />
             <span>205 $</span>
           </li>
           <li>
-            <img width={18} height={18} src="/img/cart.svg" />
+            <img width={18} height={18} src="/img/user.svg" />
           </li>
         </ul>
       </header>
