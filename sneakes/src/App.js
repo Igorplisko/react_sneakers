9c64c3122e07827072a260055e279d4ed73cd732
igorplisko <igorplisko95@gmail.com>
2021-07-05 14:49:51 +0300

    commit

--- sneakes/src/App.js
+++ sneakes/src/App.js
@@ -1,11 +1,29 @@
-import logo from './logo.svg';
+// import logo from '../public/img/logo.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      hello
-
+    <div className="wrapper">
+      <header>
+        <div className="headerLeft">
+          <img width={40} height={40} src="/img/logo.png" />
+          <svg />
+          <div className="headerInfo">
+            <h3> React Sneakers</h3>
+            <p>Shop for the best sneakers</p>
+          </div>
+        </div>
+        <ul className="headerRight">
+          <li>
+            <img width={18} height={18} src="/img/cart.svg" />
+            <span>205 $</span>
+          </li>
+          <li>
+            <img width={18} height={18} src="/img/cart.svg" />
+          </li>
+        </ul>
+      </header>
+      <div className="content"></div>
     </div>
   );
 }
