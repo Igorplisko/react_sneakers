1b4bfabed3b6cee9d8c6ab0c4688f7f1af881d71
igorplisko <igorplisko95@gmail.com>
2021-07-20 16:12:34 +0300

    commit

--- sneakes/src/App.js
+++ sneakes/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 // import './App.css';
 import Card from './components/Card/Card'
 import Header from './components/Header'
@@ -15,23 +16,30 @@ function App() {
 
 
   React.useEffect(() => {
-    fetch('https://60ed8027a78dc700178adf66.mockapi.io/items')
-      .then((res) => {
-        return res.json();
+    axios.get('https://60ed8027a78dc700178adf66.mockapi.io/items').then((res) => {
+      setItems(res.data)
+    });
 
-      }).then((json) => {
-        setItems(json)
-      });
+    axios.get('https://60ed8027a78dc700178adf66.mockapi.io/cart').then((res) => {
+      setCartItems(res.data)
+    });
   }, []);
 
+
   const onAddToCart = (obj) => {
+    axios.post('https://60ed8027a78dc700178adf66.mockapi.io/cart', obj)
     setCartItems((prev) => [...prev, obj]);
-  }
+  };
 
   const onChangeInputSearchInput = (event) => {
-    console.log(event.target.value)
+    // console.log(event.target.value)
     setSearchValue(event.target.value)
 
+  }
+
+  const onRemoveItem = (id) => {
+    // axios.delete('https://60ed8027a78dc700178adf66.mockapi.io/cart${id}')
+    setCartItems((prev) => prev.filter(item => item.id !== id));
   }
 
 
@@ -39,7 +47,7 @@ function App() {
   return (
     <div className="wrapper clear">
       {/* {cartOpened ? <CartDrawer onClose={() => setCartOpened(false)} /> : null} */}
-      {cartOpened && <CartDrawer onClose={() => setCartOpened(false)} items={cartItems} />}
+      {cartOpened && <CartDrawer onClose={() => setCartOpened(false)} items={cartItems} onRemove={onRemoveItem} />}
 
       <Header onClickCart={() => setCartOpened(true)} />
       <div className="content p-40">
@@ -47,7 +55,8 @@ function App() {
           <h1> {searchValue ? `Search by request! ${searchValue}` : 'All sneakers'} </h1>
           <div className="search-block d-flex">
             <img src="/img/search.svg" alt="Search" />
-            <input placeholder="Search..." onChange={onChangeInputSearchInput} />
+            {searchValue && < img onClick={() => setSearchValue('')} className="clear cu-p" src="/img/btn-remove.svg" alt="Clear" />}
+            <input placeholder="Search..." onChange={onChangeInputSearchInput} value={searchValue} />
           </div>
         </div>
         <div className="d-flex flex-wrap">
@@ -56,18 +65,20 @@ function App() {
 
           {/* <Card /> */}
 
-          {items.map((item, index) => (
-            <Card
-              key={item.title}
-              title={item.name}
-              price={item.price}
-              imageUrl={item.imageUrl}
-              onFavorite={() => console.log('Add to bookmarks ')}
-              // onPlus={onAddToCart}
-              onPlus={onAddToCart}
-            />
+          {items
+            .filter(item => item.title.toLowerCase().includes(searchValue))
+            .map((item, index) => (
+              <Card
+                key={index}
+                title={item.title}
+                price={item.price}
+                imageUrl={item.imageUrl}
+                onFavorite={() => console.log('Add to bookmarks ')}
+                // onPlus={onAddToCart}
+                onPlus={onAddToCart}
+              />
 
-          ))}
+            ))}
 
 
 
